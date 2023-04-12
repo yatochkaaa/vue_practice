2f66730ba7785016b6082ba538e3a65b94d40a2a
yatochkaaa <jaffplay@gmail.com>
2023-04-12 12:22:09 +0300

    test basic functions

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <button @click="addLike">Like</button>
+  <button @click="addDislike">Dislike</button>
+
+  <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+  <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    data() {
+      return {
+        likes: 0,
+        dislikes: 0,
+      }
+    },
 
-nav {
-  padding: 30px;
-}
+    methods: {
+      addLike() {
+        this.likes++
+      },
+      addDislike() {
+        this.dislikes++
+      },
+    }
+  }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
